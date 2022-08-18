54b82a5d000b6e3ae117858ae13a055b55a1c372
sufyangitrepo <muhammad.sufyan@arbisoft.com>
2022-08-18 13:09:35 +0500

    changes are done

--- eccomerce_scrap_project/eccomerce_scrap_project/items.py
+++ eccomerce_scrap_project/eccomerce_scrap_project/items.py
@@ -7,28 +7,28 @@ from itemloaders.processors import MapCompose, TakeFirst
 from scrapy.item import Field
 
 
-def add_symbol(price: str) -> str:
+def add_currency_symbol(price: str) -> str:
     symbol = '$'
     return symbol + price
 
 
-class EcommerceScrapProjectItem(scrapy.Item):
+class ProductItem(scrapy.Item):
     """
       This is an item that define the different fields these are
-        prod_category:                   it is the category of product like shoes , clothes
+        category:                   it is the category of product like shoes , clothes
         brand_name:                      it is the brand name of product
-        product_name:                    Name of the product
+        prod_name:                            Name of the product
         product_img_links                product related images links
         product_colors:                  different colors images of product
         current_price:                   it is the current price of prodcut with discount
         previous_price:                  it is the actual price of product
         off_percentage:                  the off perentage of the product
-        prod_information:                product related all information
+        detail_info:                          product related all information
         available_sizes:                 product's available sizes
 
     """
     # define the fields for your item here like:
-    prod_category = Field(
+    category = Field(
         input_processor=MapCompose(),
         output_processor=TakeFirst()
     )
@@ -47,7 +47,7 @@ class EcommerceScrapProjectItem(scrapy.Item):
         input_processor=MapCompose(),
     )
     current_price = Field(
-        input_processor=MapCompose(add_symbol),
+        input_processor=MapCompose(add_currency_symbol),
         output_processor=TakeFirst()
     )
     previous_price = Field(
@@ -58,9 +58,9 @@ class EcommerceScrapProjectItem(scrapy.Item):
         input_processor=MapCompose(),
         output_processor=TakeFirst()
     )
-    prod_information = Field(
+    detail_info = Field(
         input_processor=MapCompose(),
     )
     available_sizes = Field(
-        input_processor=MapCompose(),
+        input_processor=MapCompose()
     )
--- eccomerce_scrap_project/eccomerce_scrap_project/spiders/products.py
+++ eccomerce_scrap_project/eccomerce_scrap_project/spiders/products.py
@@ -3,12 +3,11 @@
  it will crawl the all the urls from the given domains
  based on the rules that is defined in it.
 """
-import scrapy
+
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
-from ..items import EcommerceScrapProjectItem
-from typing import Any
+from ..items import ProductItem
 
 
 class ProductsSpider(CrawlSpider):
@@ -23,13 +22,22 @@ class ProductsSpider(CrawlSpider):
     start_urls = ['https://www.6pm.com/']
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths='//header/div[@class="ja-z"]/ul/li//div/a[@class="oa-z"]')),
+        Rule(
+            LinkExtractor(
+                restrict_xpaths='//header/div[@class="ja-z"]/ul/li//div/a[@class="oa-z"]'
+            )
+        ),
         Rule(LinkExtractor(restrict_xpaths='//a[@rel="next"]')),
-        Rule(LinkExtractor(restrict_xpaths='//div[@id="products"]/article/a'), callback='parse_cats')
+        Rule(
+            LinkExtractor(
+                restrict_xpaths='//div[@id="products"]/article/a'
+            ),
+            callback='parse_cats'
+        )
     )
 
     def parse_cats(self, response):
-        product_loader = ItemLoader(item=EcommerceScrapProjectItem(), response=response)
+        product_loader = ItemLoader(item=ProductItem(), response=response)
         list_of_category = response.xpath('//div[@id = "breadcrumbs"]/div/a/text()').getall()
         product_loader.add_value('prod_category', list_of_category[1])
         product_loader.add_xpath('brand_name', '//span[@itemprop="name"]/text()')
@@ -37,11 +45,11 @@ class ProductsSpider(CrawlSpider):
         product_loader.add_xpath('product_img_links', '//*[@id="productThumbnails"]//img/@src')
         product_loader.add_xpath('product_colors', '//*[@id="buyBoxForm"]/div[1]//img/@src')
         product_loader.add_xpath('current_price', '//div[@class = "Yr-z"]/span/@content')
-        product_loader.add_xpath('previous_price', '//span[@class = "js-z"]/text()')
-        list_of_discount = response.xpath('//span[@class = "ds-z"]/span/text()').getall()
+        product_loader.add_xpath('previous_price', '//span[@class="js-z"]/text()')
+        list_of_discount = response.xpath('//span[@class="ds-z"]/span/text()').getall()
         off_percentage = ''.join(list_of_discount)
         product_loader.add_value('off_percentage', off_percentage)
-        product_loader.add_xpath('prod_information', '//div[@class = "f4-z"]/ul/li/text()')
+        product_loader.add_xpath('prod_information', '//div[@class="f4-z"]/ul/li/text()')
         product_loader.add_xpath('available_sizes',
-                                 '//form[@id = "buyBoxForm"]//div[@class = "qD-z rD-z"]/div/label/text()')
+                                 '//form[@id="buyBoxForm"]//div[@class="qD-z rD-z"]/div/label/text()')
         yield product_loader.load_item()
